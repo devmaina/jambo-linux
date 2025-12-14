d969e8f8d56eab78b2caa61df22e8afca3778776
mainadev <62764842+devmaina@users.noreply.github.com>
2025-12-14 18:59:05 +0300

    Create main.js

---
+++ web/main.js
@@ -0,0 +1,7 @@
+const msg = document.getElementById("msg");
+const btn = document.getElementById("btn");
+
+btn.onclick = () => {
+  msg.textContent =
+    "🎉 Hurray! Your first Linux challenge: What does 'ls' do?";
+};
